27d1e3d79ec78b42e32a443ce1bdc17c85a02e71
XYScience <274240671@qq.com>
2016-03-17 01:17:27 +0800

    更改图标

--- app/src/main/java/com/science/carnetplus/MainActivity.java
+++ app/src/main/java/com/science/carnetplus/MainActivity.java
@@ -173,17 +173,17 @@ public class MainActivity extends BaseActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_my) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_orders) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_car_illegally) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_exit_app) {
 
         }
 
